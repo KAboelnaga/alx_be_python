59cc104f8fe22ec10445a39b7a1ca8ca29ac995e
Kareem Aboelnaga <kaaboelnaga@gmail.com>
2025-06-13 00:36:22 +0300

    fixed strftime issue

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from datetime import timedelta
 def display_current_datetime():
-    global current_date
     current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print("Current date and time:", current_date)
 def calculate_future_date():
     number_of_days = int(input("Enter the number of days to add to the current date: "))
+    current_date = datetime.now()
     future_date = current_date + timedelta(days=number_of_days)
     future_date = future_date.strftime("%Y-%m-%d")
     print("Future date:", future_date)
